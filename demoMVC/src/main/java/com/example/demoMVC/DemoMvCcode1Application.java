442620086140708da3e654bded0d84826853fe73
VaishaliJahagirdar <vaishalijahagirdar213@gmail.com>
2022-05-20 21:33:38 +0530

    initial commit
    create MVC

---
+++ demoMVC/src/main/java/com/example/demoMVC/DemoMvCcode1Application.java
@@ -0,0 +1,13 @@
+package com.example.demoMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvCcode1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvCcode1Application.class, args);
+	}
+
+}
