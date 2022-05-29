39e2022a7bd4a99963a14ee039e9f6f9a2b3b5d9
VaishaliJahagirdar <vaishalijahagirdar213@gmail.com>
2022-05-29 21:14:58 +0530

    initial commit
    Session1

---
+++ demoSession1/src/test/java/com/example/demoSession1/DemoSession1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoSession1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSession1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
